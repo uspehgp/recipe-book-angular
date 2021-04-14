9960decaccf7ace60dc2896e4489b05e64407418
uspehgp <uspehgp@gmail.com>
2021-04-14 17:50:16 +0300

    add recipes in array

--- Angular/recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  recipes: Recipe[] = [new Recipe('Test recipe', 'Description of test recipe',
+    'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'), new Recipe('Test recipe', 'Description of test recipe',
+    'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
